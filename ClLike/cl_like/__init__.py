22073f7577095ad4e1557b3e1005c8313a5ab360
carlosggarcia <carlos.garcia-garcia@physics.ox.ac.uk>
2023-01-26 14:47:28 +0000

    added classes to init

--- ClLike/cl_like/__init__.py
+++ ClLike/cl_like/__init__.py
@@ -3,3 +3,6 @@ from . import cl_like, ccl  # noqa
 from .cl_like import ClLike, ClLikeFastBias  # noqa
 from .ccl import CCL  # noqa
 from .clccl import CLCCL  # noqa
+from .power_spectrum import Pk # noqa
+from .limber import Limber # noqa
+from .cl_final import ClFinal
