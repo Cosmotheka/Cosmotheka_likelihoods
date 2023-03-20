2154e29fdaa09bdd2150ac7e605f14c79741c1e6
David Alonso <dam.phys@gmail.com>
2023-03-20 09:58:16 +0000

    added first version of theory code

---
+++ papers/ROSATx/rosatX/__init__.py
@@ -0,0 +1,6 @@
+from .rosat import ROSATResponse
+from .profiles import (HaloProfilePressureBattaglia,
+                       HaloProfileDensityBattaglia,
+                       HaloProfileDensityNFW,
+                       HaloProfileXray,
+                       XrayTracer)
