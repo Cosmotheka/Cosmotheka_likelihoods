03e0ac971cfed6ce85c72ef0cd3e3dd73ca836af
carlosggarcia <carlos.garcia-garcia@physics.ox.ac.uk>
2023-04-27 17:35:50 +0100

    ccl: improved derived

--- ClLike/cl_like/ccl.py
+++ ClLike/cl_like/ccl.py
@@ -126,7 +126,7 @@ class CCL(Theory):
         state['derived'] = {}
         if 'A_sE9' in self.input_params:
             sigma8 = ccl.sigma8(cosmo)
-            state['derived'] = {'sigma8': sigma8}
+            state['derived']['sigma8'] = sigma8
         else:
             sigma8 = cosmo['sigma8']
 
