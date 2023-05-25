0cf6628a7add844f5846eaaaf2e36c640b337fe7
carlosggarcia <carlos.garcia-garcia@physics.ox.ac.uk>
2023-05-25 10:18:02 +0100

    improved launchers

--- ClLike/examples/launch_cobaya.sh
+++ ClLike/examples/launch_cobaya.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-export OMP_NUM_THREADS=8
+param=$1
+nodes=$2
+chains=$3
+threads=$4
+chains_per_node=$(($chains/$nodes))
+
+export OMP_NUM_THREADS=$threads
 export COBAYA_USE_FILE_LOCKING=false
 
-param=$1
+/usr/local/shared/slurm/bin/srun -N $nodes -n $chains --ntasks-per-node $chains_per_node -m cyclic --mpi=pmi2 cobaya-run $param
 
-/usr/local/shared/slurm/bin/srun -N 2 -n 6 --ntasks-per-node 3 -m cyclic --mpi=pmi2 cobaya-run $param
-# /usr/local/shared/slurm/bin/srun -n 6 --mem-per-cpu 7000 -m cyclic --mpi=pmi2 ./run_cl_cross_corr_v3_blcdm $model 10000000 $new
+# vim: tw=0
